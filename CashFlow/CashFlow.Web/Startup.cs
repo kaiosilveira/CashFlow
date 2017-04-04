85214fafba2776e3032ad9a1804c4bca0c0f6ad3
Kaio <kaiovinicios2@hotmail.com>
2017-04-04 00:46:38 -0300

    Project's structure created

---
+++ CashFlow/CashFlow.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CashFlow.Web.Startup))]
+namespace CashFlow.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
