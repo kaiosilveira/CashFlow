6454554964a3cb8700eb6e62cb8ae1ecc27d7096
Kaio <kaiovinicios2@hotmail.com>
2017-04-04 23:56:33 -0300

    Company repository interface added

---
+++ CashFlow/CashFlow.Domain/Contracts/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Domain.Model.Entities;
+using System.Collections.Generic;
+
+namespace CashFlow.Domain.Contracts.Interfaces
+{
+    public interface ICompanyRepository
+    {
+        Company Get(int id);
+
+        IEnumerable<Company> List();
+
+        int Update(Company company);
+
+        int Create(Company company);
+
+        int Delete(int id);
+    }
+}
